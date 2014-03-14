65744c260fd320358e9c763fc74097848732017a
Gavin Montague <gavin@leftbrained.co.uk>
2014-03-14 20:59:24 +0000

    properly nest the lib

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -1,4 +1,4 @@
-require_relative "./config"
-require_relative "./tip"
-require_relative "./system_state"
-require_relative "./display"
+require_relative "./reminder/config"
+require_relative "./reminder/tip"
+require_relative "./reminder/system_state"
+require_relative "./reminder/display"
